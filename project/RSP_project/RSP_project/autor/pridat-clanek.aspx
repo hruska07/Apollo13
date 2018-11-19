1dca398ae231ea82fc6a54d81c7cb14b5295848c
Lukas Jirka <jirka01@student.vspj.cz>
2018-11-19 23:01:00 +0100

    Potřeba otestovat přidávání příspěvku se souborem

--- project/RSP_project/RSP_project/autor/pridat-clanek.aspx
+++ project/RSP_project/RSP_project/autor/pridat-clanek.aspx
@@ -15,9 +15,39 @@
                 </div>
             </div>
             <div class="form-group">
+                <label for="obsah_clanku" class="control-label col-sm-1" id="Label3">Abstrakt</label>
+                <div class="col-sm-11">
+                    <asp:TextBox CssClass="form-control col-sm-10" ID="obsah_aspektu" runat="server" TextMode="MultiLine" Rows="10"></asp:TextBox>
+                </div>
+            </div>
+            <div class="form-group">
                 <label for="obsah_clanku" class="control-label col-sm-1" id="Label2">Obsah článku</label>
                 <div class="col-sm-11">
                     <asp:TextBox CssClass="form-control col-sm-10" ID="obsah_clanku" runat="server" TextMode="MultiLine" Rows="10"></asp:TextBox>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="kategorie" class="control-label col-sm-1" id="Label4">Vyberte kategorii</label>
+                <div class="col-sm-11">
+                    
+                    <asp:DropDownList ID="DropDownList1" runat="server" DataSourceID="SqlDataSource1" Font-Names="Arial" ForeColor="Black" Width="390px">
+                    </asp:DropDownList>
+                    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:LogosPolytechnikosApollo13_dbConnectionString %>" SelectCommand="SELECT * FROM [Tema]"></asp:SqlDataSource>
+                    
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="soubory" class="control-label col-sm-1" id="Label5">Přídavné soubory</label>
+                <div class="col-sm-11">
+                    
+                    <asp:Label ID="Label6" runat="server" Text="Soubor:"></asp:Label>
+                    <asp:FileUpload ID="FileUpload1" runat="server" ForeColor="Black" />
+                    <br />
+                    <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Smazat výběr" ForeColor="Black" />
+                    
+                &nbsp;&nbsp;&nbsp;
+                    <asp:CheckBox ID="CheckBox1" runat="server" Text="Nahrát soubor" />
+                    
                 </div>
             </div>
             <div class="form-group">
@@ -25,7 +55,7 @@
                     <asp:Button CssClass="btn btn-success" ID="Button1" runat="server" OnClick="Button1_Click" Text="Odeslat článek" />
                 </div>
             </div>
-            <asp:Label ID="Label3" runat="server" Text="Label" Visible="False"></asp:Label>
+            <asp:Label ID="Label7" runat="server" Text="Label" Visible="False"></asp:Label>
         </div>
     </div>
 </asp:Content>
--- project/RSP_project/RSP_project/autor/pridat-clanek.aspx.cs
+++ project/RSP_project/RSP_project/autor/pridat-clanek.aspx.cs
@@ -7,6 +7,7 @@ using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Data;
 using System.Web.Security;
+using System.IO;
 
 public partial class Zadani_prispevku : System.Web.UI.Page
 {
@@ -25,44 +26,87 @@ public partial class Zadani_prispevku : System.Web.UI.Page
 
         conn = DB.getConnection();
 
-        
     }
 
+    protected int Ulozit_soubor()
+    {
+        FileInfo fi = new FileInfo(FileUpload1.FileName);
+        byte[] documentContent = FileUpload1.FileBytes;
+
+        string name = fi.Name;
+        string extn = fi.Extension;
+
+        SqlCommand cmd = new SqlCommand("SaveDocument", conn);
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.Parameters.Add("@Name", SqlDbType.VarChar).Value = name;
+        cmd.Parameters.Add("@Content", SqlDbType.VarBinary).Value = documentContent;
+        cmd.Parameters.Add("@Extn", SqlDbType.VarChar).Value = extn;
+
+        cmd.ExecuteNonQuery();
+
+        SqlCommand get_ID = new SqlCommand("SELECT ID FROM [Documents] WHERE Name=@Name && Content=&Content && Extn=@Extn", conn);
+        int id = Convert.ToInt32(get_ID.ToString());
+
+        return id;
+    }
+   
     protected void Button1_Click(object sender, EventArgs e)
     {
+        int soubor = 0;
         //textové pole nadpis
         string Nadpis = nadpis_clanku.Text;
 
         //textové pole obsahu
         string Obsah = obsah_clanku.Text;
 
+        string Abstrakt = obsah_aspektu.Text;
         //aktuální čas a datum
         DateTime date1 = DateTime.Now;
 
         //příkaz do databáze
-        SqlCommand insert = new SqlCommand("insert into [Clanek] (nadpis_clanku,obsah_clanku, datum_clanku,autor,stav) values(@Nadpis,@Obsah, @datum_clanku,@autor,@stav)", conn);
+        if (CheckBox1.Checked == true)
+        {
+            soubor = Ulozit_soubor();
+        }
+        else
+        {
+            soubor = 0; 
+        }
+
+        SqlCommand insert = new SqlCommand("insert into [Clanek] (nadpis_clanku,obsah_clanku, datum_clanku,autor,stav,tema,soubor,abstrakt) values(@Nadpis,@Obsah, @datum_clanku,@autor,@stav,@tema,@soubor,@abstrakt)", conn);
         insert.Parameters.AddWithValue("@Nadpis", Nadpis);
         insert.Parameters.AddWithValue("@Obsah", Obsah);
+        insert.Parameters.AddWithValue("@abstrakt", Abstrakt);
         insert.Parameters.AddWithValue("@datum_clanku", date1);
         insert.Parameters.AddWithValue("@autor", cislo_autora);
         insert.Parameters.AddWithValue("@stav", 1);
+        insert.Parameters.AddWithValue("@tema", DropDownList1.SelectedIndex.ToString());
+        insert.Parameters.AddWithValue("@soubor", soubor);
 
         try
         {
             //vložení do databáze
             insert.ExecuteNonQuery();
 
-            Label3.Visible = true;
-            Label3.ForeColor = System.Drawing.Color.Green;
-            Label3.Text = "Příspěvek byl úspěšně přidán";
+            Label7.Visible = true;
+            Label7.ForeColor = System.Drawing.Color.Green;
+            Label7.Text = "Příspěvek byl úspěšně přidán";
             //Response.Redirect("/default");
         }
         catch (Exception ex)
         {
-            Label3.Visible = true;
-            Label3.ForeColor = System.Drawing.Color.Red;
-            Label3.Text = "Error: " + ex.Message;
+            Label7.Visible = true;
+            Label7.ForeColor = System.Drawing.Color.Red;
+            Label7.Text = "Error: " + ex.Message;
             //Response.Redirect("/default");
         }
+
     }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        FileUpload1.FileContent.Dispose();
+    }
+
+
 }
