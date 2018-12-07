d78d0e07192274b22328a026966cf3bc811abf90
Lukas Jirka <43777703+Daikiro@users.noreply.github.com>
2018-12-07 20:51:21 +0100

    Oprava stahování ze složky

--- project/RSP_project/RSP_project/clanky.aspx.cs
+++ project/RSP_project/RSP_project/clanky.aspx.cs
@@ -50,42 +50,17 @@ public partial class _Default : System.Web.UI.Page
     {
         int id_clanek = Convert.ToInt32(Request.RawUrl.ToString().Substring(10));
         DataRow clanek = DB.getClanekById(id_clanek);
-        int id = 0;
-        try
+        string path = clanek["path"].ToString();
+        string name = clanek["nadpis_clanku"].ToString();
+
+        if (path != "")
         {
-             id = Convert.ToInt32(clanek["soubor"].ToString());
+            Response.Clear();
+            Response.ContentType = "application/octet-stream";
+            Response.AppendHeader("content-disposition", "filename=" + name);
+            Response.TransmitFile(path);
+            Response.End();
         }
-        catch (Exception)
-        {
-            Button1.Enabled = false;
-        }
-
-        if (Button1.Enabled == true && id!= 0)
-        {
-            DataTable dt = new DataTable();
-
-             cmd = new SqlCommand("GetDocument", conn);
-             cmd.CommandType = CommandType.StoredProcedure;
-             cmd.Parameters.Add("@ID", SqlDbType.Int).Value = id;
-             SqlDataReader reader = cmd.ExecuteReader();
-
-             dt.Load(reader);
-
-
-            string name = dt.Rows[0]["Name"].ToString();
-            byte[] documentBytes = (byte[])dt.Rows[0]["DocumentContent"];
-
-            Response.ClearContent();
-            Response.ContentType = "Application/octetstream";
-            Response.AddHeader("Content-Disposition", string.Format("attachment; filename={0}", name));
-            Response.AddHeader("Content-Lenght", documentBytes.Length.ToString());
-
-            Response.BinaryWrite(documentBytes);
-            Response.Flush();
-            Response.Close();
-        }
-        
-
 
     }
 }
