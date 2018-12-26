aa16027d6d39e78c4423a10e818235469881427d
Dominik Bula <d.bula@seznam.cz>
2018-12-26 20:15:04 +0100

    sefredaktor -agenda autora

---
+++ project/RSP_project/RSP_project/sefredaktor/agenda-autora.aspx.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public partial class Agenda_Redakor : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if ((Session["id_user"] == null) || (Session["nazev_role"].ToString() != "sefredaktor"))
+            Response.Redirect("/login");
+    }
+}
