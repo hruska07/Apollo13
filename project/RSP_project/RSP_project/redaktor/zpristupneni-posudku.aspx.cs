fe05b33305ac128e626a3395fa64e2c37afb03d3
Ondrej Vankat <deextaar@gmail.com>
2019-01-03 23:25:23 +0100

    Zpristupenni posudku - nejaka cvhyb JOIN, ještě opravím

--- project/RSP_project/RSP_project/redaktor/zpristupneni-posudku.aspx.cs
+++ project/RSP_project/RSP_project/redaktor/zpristupneni-posudku.aspx.cs
@@ -50,27 +50,47 @@ public partial class redaktor_zpristupneni_posudku : System.Web.UI.Page
         Label3.Visible = true;
         Label4.Visible = true;
         CheckBox1.Visible = true;
-        Button1.Visible = true; 
+        Button1.Visible = true;
+        Label6.Visible = true;
+        Label7.Visible = true;
 
 
-      
-            idecko = GridView13.SelectedRow.Cells[1].Text;
-            cmd = new SqlCommand("SELECT zpristupnen,komentar FROM Posudek WHERE id_posudek=@id_posude", conn);
+        idecko = GridView13.SelectedRow.Cells[1].Text;
+            cmd = new SqlCommand("SELECT zpristupnen,doplnujici_komentar,namety_k_diskuzi FROM Posudek WHERE id_posudek=@id_posude", conn);
             cmd.Parameters.AddWithValue("@id_posude", idecko);
             dr = cmd.ExecuteReader();
             dr.Read();
             
             CheckBox1.Checked = Convert.ToBoolean(dr[0]);
             TextBox1.Text = dr[1].ToString();
-           
-            dr.Close();
-            cmd.Dispose();
-        
-            TextBox1.Visible = true;
+        TextBox2.Text = dr[2].ToString();
 
-          
 
-        
+        dr.Close();
+        cmd.Dispose();
+        cmd = new SqlCommand("SELECT [Souhrnne_vyjadreni].text_vyjadreni FROM [Posudek] JOIN [Souhrnne_vyjadreni] ON [Posudek].souhrnne_vyjadreni=[Souhrnne_vyjadreni].id_vyjadreni WHERE id_posudek=@id_posude", conn);
+        cmd.Parameters.AddWithValue("@id_posude", idecko);
+        dr = cmd.ExecuteReader();
+        dr.Read();
+
+        TextBox3.Text = dr[0].ToString();
+      
+
+
+        dr.Close();
+        cmd.Dispose();
+
+
+
+
+        TextBox1.Visible = true;
+        TextBox2.Visible = true;
+        TextBox3.Visible = true;
+
+     
+
+
+
     }
 
     protected void TextBox1_TextChanged(object sender, EventArgs e)
@@ -85,11 +105,19 @@ public partial class redaktor_zpristupneni_posudku : System.Web.UI.Page
         zpristupnen = CheckBox1.Checked;
         komentar = TextBox1.Text;
 
-          SqlCommand update = new SqlCommand("update Posudek set zpristupnen=@zpristupnen,komentar=@komentar Where id_posudek=@id_posudek", conn);
+          SqlCommand update = new SqlCommand("update Posudek set zpristupnen=@zpristupnen,doplnujici_komentar=@doplnujici_komentar Where id_posudek=@id_posudek", conn);
         update.Parameters.AddWithValue("@zpristupnen", zpristupnen);
-        update.Parameters.AddWithValue("@komentar", komentar);
+        update.Parameters.AddWithValue("@doplnujici_komentar", komentar);
         update.Parameters.AddWithValue("@id_posudek", GridView13.SelectedValue);
+update.ExecuteNonQuery();
+
+
+        update = new SqlCommand("UPDATE [Souhrnne_vyjadreni] JOIN [Posudek] ON [Souhrnne_vyjadreni].id_vyjadreni=[Posudek].souhrnne_vyjadreni SET [Souhrnne_vyjadreni].text_vyjadreni=@vyjadreni WHERE [Posudek].id_posudek=@id_posudek", conn);
+        update.Parameters.AddWithValue("@vyjadreni",TextBox3.Text);
+
         update.ExecuteNonQuery();
+
+
         Response.Redirect(Request.RawUrl);
        
 
