269c493753fe1c55014041beaad797d378e897f3
Ondrej Vankat <deextaar@gmail.com>
2019-01-04 17:47:56 +0100

    helpdesk alpha version

--- project/RSP_project/RSP_project/helpdesk/helpdesk.aspx.cs
+++ project/RSP_project/RSP_project/helpdesk/helpdesk.aspx.cs
@@ -12,6 +12,9 @@ public partial class helpdesk : System.Web.UI.Page
     Database DB = new Database();
     SqlCommand cmd;
     SqlConnection conn = null;
+  //  SqlDataReader reader;
+  
+  
     protected void Page_Load(object sender, EventArgs e)
     {
         conn = DB.getConnection();
@@ -24,13 +27,35 @@ public partial class helpdesk : System.Web.UI.Page
 
     protected void Button1_Click(object sender, EventArgs e)
     {
-       bool prom = false;
+       
+           
+        bool prom = false;
         SqlCommand insert = new SqlCommand("insert into [Helpdesk] (obsah_helpdesk,jmeno,email,vyrizeno) values (@obsah_helpdesk,@jmeno,@email,@prom)", conn);
         insert.Parameters.AddWithValue("@obsah_helpdesk", TextBox3.Text);
         insert.Parameters.AddWithValue("@jmeno", TextBox1.Text);
         insert.Parameters.AddWithValue("@email", TextBox2.Text);
         insert.Parameters.AddWithValue("@prom", prom);
         insert.ExecuteNonQuery();
+        insert.Dispose();
+        string message2 = "Nový dotaz na Helpdesku !";
+
+        cmd = new SqlCommand("SELECT id_user FROM [User] WHERE role=4 OR role=5 OR role=6  ", conn);
+        SqlDataReader reader = cmd.ExecuteReader();
+       int promena ;
+      
+        while (reader.Read())
+        {
+            promena = Convert.ToInt32(reader["id_user"]);
+
+
+            DB.insertNotification(promena,message2);
+
+        }
+        //    DB.insertNotification(oponent, message2);
+        reader.Close();
+        cmd.Dispose();
+
+
         Response.Redirect(Request.RawUrl);
     }
 }
--- project/RSP_project/RSP_project/helpdesk/helpdesk_zobrazeni.aspx.cs
+++ project/RSP_project/RSP_project/helpdesk/helpdesk_zobrazeni.aspx.cs
@@ -57,27 +57,8 @@ public partial class helpdesk_zobrazeni : System.Web.UI.Page
 
 
         email.sendEmail(mail, "Helpdesk - odpověď", TextBox2.Text);
-        /*
-        //notifikace - mail - autor
-        DataRow clanek = DB.getClanekById(Convert.ToInt32(GridView1.SelectedValue.ToString()));
-        DataRow user = DB.getUserById(Convert.ToInt32(clanek["autor"].ToString()));
-        string message = "Stav vašeho článku '" + clanek["nadpis_clanku"] + "' byl změněn, byl přidělen oponent. Aktuální stav: Čeká na posudek";
-        nf.sendEmail(user["email"].ToString(), "Článek - změna stavu", message);
-        //notifikace - stranky - autor
-        DB.insertNotification(int.Parse(clanek["autor"].ToString()), int.Parse(clanek["id_clanek"].ToString()), "info", message);
+  
 
-        //notifikace - mail (oponenti)
-        string message2 = "K recenzi Vám byl přidělen nový článek: " + clanek["nadpis_clanku"];
-        DataRow oponent1_DB = DB.getUserById(oponent);
-        DataRow oponent2_DB = DB.getUserById(oponent2);
-        nf.sendEmail(oponent1_DB["email"].ToString(), "Recenzní řízení - nový článek", message2);
-        nf.sendEmail(oponent2_DB["email"].ToString(), "Recenzní řízení - nový článek", message2);
-
-        //notifikace - stranky (oponenti)
-        DB.insertNotification(oponent, int.Parse(clanek["id_clanek"].ToString()), "info", message2);
-        DB.insertNotification(oponent2, int.Parse(clanek["id_clanek"].ToString()), "info", message2);
-
-    */
 
 
 
